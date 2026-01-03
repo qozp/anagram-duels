746252a5d224cfa2fc662ae341a9c35159b38d3c
Z <isbpham@gmail.com>
2026-01-03 13:04:28 -0500

    Initial Commit

---
+++ anagram-duels/anagram_duelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  anagram_duelsApp.swift
+//  anagram-duels
+//
+//  Created by Isaiah Pham on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct anagram_duelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
