fa8309e63ba527da50f37fc42c5342f31f2f06e0
Z <isbpham@gmail.com>
2026-01-03 13:04:28 -0500

    Initial Commit

---
+++ anagramduels/anagramduels/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  anagramduels
+//
+//  Created by Isaiah Pham on 2/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ anagramduels/anagramduels/anagramduelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  anagramduelsApp.swift
+//  anagramduels
+//
+//  Created by Isaiah Pham on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct anagramduelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
